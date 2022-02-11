65d6f3ac4141044f089bb5603b93644fb189e7c8
Shaikh Islam <shaikhalvee@gmail.com>
2022-02-11 18:24:15 -0500

    modified quicksort added

---
+++ ModifiedQuickSort.java
@@ -0,0 +1,87 @@
+package com.project;
+
+public class ModifiedQuickSort {
+	public static void doModifiedQuickSort(int[] array) {
+		quickSort(array, 0, array.length - 1);
+	}
+
+	private static void quickSort(int[] array, int left, int right) {
+		if (left + 8 < right) {
+			int partitionIndex = partitionWithMedian(array, left, right);
+			quickSort(array, left, partitionIndex - 1);
+			quickSort(array, partitionIndex + 1, right);
+		} else {
+			InsertionSort.doInsertionSort(array);
+		}
+	}
+
+	private static int partitionWithMedian(int[] array, int left, int right) {
+		// taking last or rightmost index as pivot
+		int pivot = medianOfArray(array, left, right);
+		int leftIndex = left;
+		int rightIndex = right - 2;
+		int swap;
+		while (leftIndex < rightIndex) {
+//			while (array[leftIndex++])
+			// not using leftIndex++ because, current leftIndex has already been
+			// compared in the median calculations
+			while (array[++leftIndex] < pivot) ;
+			while (rightIndex > 0 && array[--rightIndex] > pivot) ;
+			if (leftIndex >= rightIndex) {
+				break;
+			} else {
+				swap = array[leftIndex];
+				array[leftIndex] = array[rightIndex];
+				array[rightIndex] = swap;
+			}
+		}
+		swap = array[leftIndex];
+		array[leftIndex] = array[right - 1];
+		array[right - 1] = swap;
+		return leftIndex;
+	}
+
+	private static int medianOfArray(int[] array, int left, int right) {
+		int median = left / 2 + right / 2;
+		int smaller, middle, larger;
+		if (array[left] <= array[right]) {
+			if (array[left] <= array[median]) {
+				smaller = array[left];
+				if (array[median] <= array[right]) {
+					middle = array[median];
+					larger = array[right];
+				} else {
+					middle = array[right];
+					larger = array[median];
+				}
+			} else {
+				middle = array[left];
+				smaller = array[median];
+				larger = array[right];
+			}
+		} else {
+			if (array[median] <= array[right]) {
+				smaller = array[median];
+				middle = array[right];
+				larger = array[left];
+			} else if (array[median] <= array[left]) {
+				smaller = array[right];
+				middle = array[median];
+				larger = array[left];
+			} else {
+				smaller = array[right];
+				middle = array[left];
+				larger = array[median];
+			}
+		}
+		array[left] = smaller;
+		array[right] = larger;
+
+		// setting pivot at right - 1
+		int temp = array[right - 1];
+		array[right - 1] = middle;
+		array[median] = temp;
+		return middle;
+	}
+
+}
--- SortingTechniqueComparison.java
+++ SortingTechniqueComparison.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 
 public class SortingTechniqueComparison {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        int[] ara = {3, 0, -4, 2, 7, 5, 4, 34, -3, -1, 0, 8, 9, 22, 11, 2, 7, 8};
+		int[] ara = {3, 0, -4, 2, 7, 5, 4, 34, -3, -1, 0, 8, 9, 22, 11, 2, 7, 8};
+		int[] ara2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 //        InsertionSort.doInsertionSort(ara);
-        QuickSort.doQuickSort(ara);
-        System.out.println(Arrays.toString(ara));
-    }
+//		QuickSort.doQuickSort(ara);
+        ModifiedQuickSort.doModifiedQuickSort(ara);
+		System.out.println(Arrays.toString(ara));
+	}
 
 }
