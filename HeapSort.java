82f28c210ea29bcf77e22607ba54dcfbd4b85153
Shaikh Islam <shaikhalvee@gmail.com>
2022-02-12 12:39:25 -0500

    heapsort added

---
+++ HeapSort.java
@@ -0,0 +1,34 @@
+package com.project;
+
+public class HeapSort {
+
+	public static void doHeapSort(int[] array) {
+		for (int i = array.length / 2 - 1; i >= 0; i--) {
+			buildMinHeap(array, array.length, i);
+		}
+		for (int i = array.length - 1; i >= 0; i--) {
+			int root = array[0];
+			array[0] = array[i];
+			array[i] = root;
+			buildMinHeap(array, i, 0);
+		}
+	}
+
+	private static void buildMinHeap(int[] array, int heapLength, int index) {
+		int rootIndex = index;
+		int leftIndex = (index << 1) + 1;
+		int rightIndex = (index << 1) + 2;
+		if (leftIndex < heapLength && array[leftIndex] > array[rootIndex]) {
+			rootIndex = leftIndex;
+		}
+		if (rightIndex < heapLength && array[rightIndex] > array[rootIndex]) {
+			rootIndex = rightIndex;
+		}
+		if (rootIndex != index) {
+			int swap = array[rootIndex];
+			array[rootIndex] = array[index];
+			array[index] = swap;
+			buildMinHeap(array, heapLength, rootIndex);
+		}
+	}
+}
